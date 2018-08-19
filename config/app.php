74029723ed38c9e04728806b4664ac8d6aac8f56
anarzone <haqverdiyevanar@gmail.com>
2018-08-19 23:39:14 +0400

    update app name in app.php

--- config/app.php
+++ config/app.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'name' => env('APP_NAME', 'traversyblog'),
+    'name' => env('APP_NAME', 'Modernizer'),
 
     /*
     |--------------------------------------------------------------------------
